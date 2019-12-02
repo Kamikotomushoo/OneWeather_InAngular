63d274f2a381de85770e98ae7d2f692eaeeed173
Daniil Shlive <kavaki258@gmail.com>
2019-12-02 09:30:46 +0200

    version 4.0 of my normal weather app

--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -2,7 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient  } from '@angular/common/http';
 import {WeatherContext} from '../interfaces/weather-context';
-
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class WeatherApiService {
@@ -15,7 +15,7 @@ export class WeatherApiService {
   getWeather(cityName: string)
   {
 
-     return this.http.get('https://api.openweathermap.org/data/2.5/weather?q='
+     return this.http.get(environment.apiUrl + '/weather?q='
      + cityName + '&appid=5d60c80406f8941039d85837e08019a1');
 
   }
