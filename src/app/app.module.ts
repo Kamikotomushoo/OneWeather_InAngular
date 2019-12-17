e504fc3494b3e4c3f4be3f9cd3cb640997dc67e0
Daniil Shlive <kavaki258@gmail.com>
2019-12-17 13:55:06 +0200

    final version of ONEWEATHER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InputFormComponent } from './components/main.page/input-form/input-form.component';
 import { MainResponseComponent } from './components/main.page/main-response/main-response.component';
 import { WeatherApiService } from './services/weather-api.service';
-import { HttpClientModule } from '@angular/common/http';
 import { WelcomePageComponent } from './components/welcome.page/welcome-page/welcome-page.component';
 import { MainPageComponent } from './components/main.page/main-page.component';
 import { NotFoundComponent } from './components/not-found.page/not-found/not-found.component';
